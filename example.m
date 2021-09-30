5adb00770c5597659b2f44a0b0f92ca80e1603f3
Linxiaobuskiy <51104536+Linxiaobuskiy@users.noreply.github.com>
2021-09-30 13:20:50 +0800

    Update example.m

--- example.m
+++ example.m
@@ -1,31 +1,42 @@
-
 clear all
+%% input data
+%{
+% scRef: scRNA-seq reference data
 scRef = readtable('ReferenceSC.csv','ReadRowNames',true);
+% bulkExp: bulk RNA-seq data
 bulkExp = readtable('Bulk.csv','ReadRowNames',true);
+%}
+
+% Considering the data size limitation in GitHub
+% we prepare 'T.mat' to replace scRef and bulkExp
+load('T.mat');
+
+% CellLabel: Cell annotations of single cells in scRef
 CellLabel = readtable('InfoSC.csv','ReadRowNames',true);
+% gene_len: The number of bases of each gene
 gene_len = readtable('gene_len.txt','ReadRowNames',true);
 
 %% scRNA to Bulk
-scBulk = scRNA2Bulk(scRef);
+scBulk = scRNA2Bulk(scRef); % construct pseudo bulk RNA-seq data from scRNA-seq data
 tmpBulk = CellCompositionStatistic1(CellLabel);
 scBulk.RealComposition = tmpBulk.composition;
 scBulk.RealCellNumbers = tmpBulk.cellnumbers;
 clear tmpBulk
 
 %% input data prepare
-data = DataPretreatment(scRef,scBulk.matrix);
-data.RealComposition = scBulk.RealComposition;
-data.RealCellNumbers = scBulk.RealCellNumbers;
+data = DataPretreatment(scRef,scBulk.matrix);  % construct input data for deconvolution, containing reference matrix and Bulk expression matrix
+data.RealComposition = scBulk.RealComposition; % not necessary
+data.RealCellNumbers = scBulk.RealCellNumbers; % not necessary
 
 
 %% PseudoSC deconvolution
-para.vargenesize = 150;
-para.m = 10000;
-para.cores = 12;
-para.times = 120;
-para.mode = 'mosek';
+para.vargenesize = 150 ;    % parametric genes numbers
+para.m = 10000;             % single cell sampling size in each process
+para.cores = 12;            % cores used in parallel computing
+para.times = 120;           % repeat times
+para.mode = 'mosek';        % solver selection
 
-result = PseudoSC(data,para,gene_len);
+result = PseudoSC(data,para,gene_len); % result.X contains the deconvolution result. X[i,j] means the estimated cell number of cell j in sample i.
 % cell composition
 result = CellCompositionStatistic2(result,CellLabel);
 
